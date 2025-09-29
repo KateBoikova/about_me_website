f57d6c07421f3c300a4225cc230ae8f7758bb4a4
Kateryna Boikova <boykova.ekaterina.i@gmail.com>
2025-09-29 14:00:44 +0100

    improve typing, minor fixes

---
+++ index.js
@@ -0,0 +1,17 @@
+const introText =
+  "Hi! My name is Kateryna Boikova. I'm a professional translator seeking career change opportunity in a fascinating world of web development.";
+
+// const typingSpeed = 5000;
+
+const introTypewriter = document.getElementById("introTypewriter");
+
+function typewriter(text, index) {
+  if (index < text.length) {
+    introTypewriter.innerHTML += text.charAt(index);
+    setTimeout(() => {
+      typewriter(text, index + 1);
+    }, 50);
+  }
+}
+
+typewriter(introText, 0);
